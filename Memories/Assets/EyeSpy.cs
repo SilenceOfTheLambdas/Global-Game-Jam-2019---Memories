2836874e1f8d1a0f6fc4591e2f193f3ae9b6ad94
Smith <cs18804@essex.ac.uk>
2019-01-26 02:43:23 +0000

    implemented Game Manager, score

---
+++ Memories/Assets/EyeSpy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class EyeSpy : MonoBehaviour
+{
+
+    [SerializeField] private GameObject item;
+    public int tries;
+
+    void Start()
+    {
+        if (GlobalControl.Instance.boardGamePickedUp)
+        {
+            Destroy(item);
+        }        
+    }
+
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        Destroy(item);
+        GlobalControl.Instance.boardGamePickedUp = true;
+        GlobalControl.Instance._score++;
+    }
+}
---
+++ Memories/Assets/GlobalControl.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GlobalControl : MonoBehaviour {
+
+    public static GlobalControl Instance;
+
+    [Header("The Global Item Score")]
+    public int _score; // Stores the global score
+
+    public Text counter; // The counter text
+    public Text winText; // The win text
+
+    public bool boardGamePickedUp = false; // Has the board game been picked up?
+    public bool magnifyingGlassPickedUp = false; // Has the magnifying glass been picked up?
+    public bool penPickedUp = false; // Has the pen been picked up?
+    public bool journalPickedUp = false; // Has the journal been picked up?
+    public bool applePickedUp = false; // Has the apple been picked up?
+    public bool chocolateBarPickedUp = false; // Has the chocolate bar been picked up?
+    public bool skippingRopePickedUp = false; // Has the skipping rope been picked up?
+
+    void FixedUpdate()
+    {
+        SetCountText();
+        winText.text = "";
+    }
+
+    void SetCountText()
+    {
+        counter.text = "Count: " + _score.ToString();
+    }
+
+    void ObjectsCollectedDestroy()
+    {
+        if (boardGamePickedUp)
+        {
+            Destroy(GameObject.FindWithTag("BoardGame"));
+        }
+
+        if (penPickedUp)
+        {
+            Destroy(GameObject.FindWithTag("Pen"));
+        }
+        if (journalPickedUp)
+        {
+            Destroy(GameObject.FindWithTag("Jpurnal"));
+        }
+        if (applePickedUp)
+        {
+            Destroy(GameObject.FindWithTag("Apple"));
+        }
+        if (chocolateBarPickedUp)
+        {
+            Destroy(GameObject.FindWithTag("ChocolateBar"));
+        }
+
+        if (skippingRopePickedUp)
+        {
+            Destroy(GameObject.FindWithTag("SkippingRope"));
+        }
+    }
+
+    void onGameOver()
+    {
+        if (_score == 1)
+        {
+            counter.text = "";
+            winText.text = "YOU WIN!";
+        }
+    }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
